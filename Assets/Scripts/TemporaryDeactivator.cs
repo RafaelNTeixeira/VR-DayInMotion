94b7ca27172ba7bbd9074b3a091a0c873a527610
Joao Costa <jpmoreiracosta10@gmail.com>
2025-12-11 19:41:21 +0000

    Added the button and fiz the flying cars

---
+++ Assets/Scripts/TemporaryDeactivator.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class TemporaryDeactivator : MonoBehaviour
+{
+    [Header("Settings")]
+    [Tooltip("Drag the object you want to disable here")]
+    public GameObject objectToDeactivate;
+
+    [Tooltip("Time in seconds to stay deactivated")]
+    public float duration = 60f; 
+
+    // Link this method to your Button's OnClick event
+    public void DeactivateObject()
+    {
+        Debug.Log("DeactivateObject method called.");
+        if (objectToDeactivate != null)
+        {
+            StartCoroutine(DisableRoutine());
+        }
+        else
+        {
+            Debug.LogError("No object assigned to deactivate!");
+        }
+    }
+
+    private IEnumerator DisableRoutine()
+    {
+        // 1. Deactivate the object
+        objectToDeactivate.SetActive(false);
+
+        // 2. Wait for the specified duration (60 seconds)
+        yield return new WaitForSeconds(duration);
+
+        // 3. Reactivate the object
+        objectToDeactivate.SetActive(true);
+    }
+}
