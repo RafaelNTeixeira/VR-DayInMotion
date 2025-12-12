677952b856bb173ad790f39d8e68e044c8bb7af0
RafaelNTeixeira <rafael2003t.18@gmail.com>
2025-12-12 13:05:41 +0000

    Improved menu

--- Assets/Scripts/VRMainMenu.cs
+++ Assets/Scripts/VRMainMenu.cs
@@ -7,15 +7,28 @@ public class VRMainMenu : MonoBehaviour
     [Tooltip("The name or index of the game scene to load")]
     [SerializeField] private string gameSceneName = "GameScene"; 
 
+    [Tooltip("The sound clip to play when a button is clicked.")]
+    [SerializeField] private AudioClip clickSound;
+
     public void OnPlayClicked()
     {
         Debug.Log("Play Button Clicked - Loading Game...");
+        if (clickSound != null)
+        {
+            AudioSource.PlayClipAtPoint(clickSound, transform.position);
+        }
+
         SceneManager.LoadScene(gameSceneName);
     }
 
     public void OnQuitClicked()
     {
         Debug.Log("Quit Button Clicked - Exiting Application.");
+
+        if (clickSound != null)
+        {
+            AudioSource.PlayClipAtPoint(clickSound, transform.position);
+        }
         
         Application.Quit();
 
